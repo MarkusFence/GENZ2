62d1616ddd5157d2e84cde0f1beb2e1b948acf82
Markuz <mark.ohradka@gmail.com>
2025-03-15 20:37:27 +0100

    optimalzation, detection in progress

--- src/dispGra.h
+++ src/dispGra.h
@@ -2,6 +2,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 
+//#include "deb.h"
+
 //==========================================================================//
 //------------------------- DISLPAY SETUP
 //==========================================================================//
@@ -11,8 +13,8 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(6, 5, 4);
 //graphical digit pointer 
 int cursorPosition = 1;
 
-//GRAPHICS
-void graphisc_print(boolean enable_output){
+//GRAPHICS FOR CURRENT SOURCE
+void graphisc_print_CurrSource(boolean enable_output){
 
     int digit_1;
     float digit_2;
@@ -47,6 +49,7 @@ void graphisc_print(boolean enable_output){
     }
 
     display.setTextSize(2);
+
     display.setCursor(38, 8);
     display.print(".");
     display.setCursor(60, 32);
@@ -60,6 +63,17 @@ void graphisc_print(boolean enable_output){
     }
 
     display.setTextSize(1);
+
+    // display.setCursor(13, 28);
+    // display.print("IMPEDANCE");
+    // display.setCursor(30, 39);
+    // display.print("HIGH");
+
+    display.setCursor(21, 28);
+    display.print("BELOW");
+    display.setCursor(21, 39);
+    display.print("LIMIT");
+
     if(enable_output){
     display.fillCircle( 6, 41, 4, BLACK);
     display.drawCircle( 6, 41, 6, BLACK);
@@ -67,6 +81,86 @@ void graphisc_print(boolean enable_output){
     display.drawCircle( 6, 41, 6, BLACK);
     }
 
+
+    display.display();
+}
+
+void graphics_print_VoltSource(boolean enable_output){
+
+    display.clearDisplay();
+    //display.setTextColor(WHITE,BLACK);
+
+    
+    int v_digit_1;
+    float v_digit_2;
+    float voltage_value_absolute = voltage_value;
+
+    if(voltage_value < 0){
+    voltage_value_absolute *= -1;
+    // minus graphic
+    display.drawLine(0, 10, 4, 10, BLACK);
+    display.drawLine(0, 11, 4, 11, BLACK);
+    };
+
+    //digit calculation before print
+    v_digit_1 = (int)voltage_value_absolute;
+    v_digit_2 = ((float)voltage_value_absolute - v_digit_1) * 100; 
+
+    display.setTextSize(3);
+    
+    if(v_digit_1 < 10){
+    display.setCursor(7, 0);
+    display.print("0");
+    display.setCursor(25, 0);
+    display.print(v_digit_1);
+    }else{
+    display.setCursor(7, 0);
+    display.print(v_digit_1);
+    }
+
+    if(v_digit_2 < 10){
+    display.setCursor(48, 0);
+    display.print("0");
+    display.setCursor(66, 0);
+    display.print((int)v_digit_2);
+    display.setCursor(0, 0);
+    }else{
+    display.setCursor(48, 0);
+    display.print((int)v_digit_2);
+    } //dot graphic
+    display.setTextSize(2);
+    display.setCursor(38, 8);
+    display.print(".");
+    //source option
+    display.setCursor(72, 32);
+    display.print("V");
+    
+    if(cursorPosition == 1){
+    display.drawLine(24, 24, 39, 24, BLACK);
+    }
+    else{
+    display.drawLine(66, 24, 81, 24 ,BLACK);
+    }
+
+    display.setTextSize(1);
+
+    display.setCursor(13, 28);
+    display.print("IMPEDANCE");
+    display.setCursor(30, 39);
+    display.print("LOW");
+
+
+
+    if(enable_output){
+    display.fillCircle( 6, 41, 4, BLACK);
+    display.drawCircle( 6, 41, 6, BLACK);
+    }else{
+    display.drawCircle( 6, 41, 6, BLACK);
+    }
+
+    // for(int i = 0; i > 10; i++){
+    // PRINT(" ");
+    // }
 
     display.display();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,6 @@
 #include "deb.h"
 
 //==========================================================================//
-//------------------------- DISLPAY SETUP
-//==========================================================================//
-// #define DCONST 100
-// Adafruit_PCD8544 display = Adafruit_PCD8544(6, 5, 4);
-
-
-//==========================================================================//
 //-------------------------MENU SETUP
 //==========================================================================//
 // select the enum with all the menu pages option
@@ -42,8 +35,6 @@ void setup() {
   Serial.println("DEBBUG BEGIN !!!");
   //short version of serial print
   PRINT("YEEEEEET");
-
-  pinMode(LED_BUILTIN, OUTPUT);
 
   //SPI setup
   SPI.begin();                                                     // Initialize SPI
@@ -111,73 +102,7 @@ void page_VoltSettings(void)
     if (updateDisplay)
     {
       updateDisplay = false;
-      display.clearDisplay();
-      //display.setTextColor(WHITE,BLACK);
-
-      
-      int v_digit_1;
-      float v_digit_2;
-      float voltage_value_absolute = voltage_value;
-
-      if(voltage_value < 0){
-        voltage_value_absolute *= -1;
-        // minus graphic
-        display.drawLine(0, 10, 4, 10, BLACK);
-        display.drawLine(0, 11, 4, 11, BLACK);
-      };
-
-      //digit calculation before print
-      v_digit_1 = (int)voltage_value_absolute;
-      v_digit_2 = ((float)voltage_value_absolute - v_digit_1) * 100; 
-  
-      display.setTextSize(3);
-      
-      if(v_digit_1 < 10){
-        display.setCursor(7, 0);
-        display.print("0");
-        display.setCursor(25, 0);
-        display.print(v_digit_1);
-      }else{
-        display.setCursor(7, 0);
-        display.print(v_digit_1);
-      }
-
-      if(v_digit_2 < 10){
-        display.setCursor(48, 0);
-        display.print("0");
-        display.setCursor(66, 0);
-        display.print((int)v_digit_2);
-        display.setCursor(0, 0);
-      }else{
-        display.setCursor(48, 0);
-        display.print((int)v_digit_2);
-      } //dot graphic
-        display.setTextSize(2);
-        display.setCursor(38, 8);
-        display.print(".");
-        //source option
-        display.setCursor(72, 32);
-        display.print("V");
-      
-      if(cursorPosition == 1){
-        display.drawLine(24, 24, 39, 24, BLACK);
-      }
-      else{
-        display.drawLine(66, 24, 81, 24 ,BLACK);
-      }
-
-      display.setTextSize(1);
-      if(enable_output){
-        display.fillCircle( 6, 41, 4, BLACK);
-        display.drawCircle( 6, 41, 6, BLACK);
-      }else{
-        display.drawCircle( 6, 41, 6, BLACK);
-      }
-
-      for(int i = 0; i > 10; i++){
-      PRINT(" ");
-      }
-      display.display();
+      graphics_print_VoltSource(enable_output);
     }
     
     //BUTTOM UTILITIES
@@ -309,7 +234,7 @@ void page_CurrSettings(void)
     {
       updateDisplay = false;
       
-      graphisc_print(enable_output);
+      graphisc_print_CurrSource(enable_output);
     }
     
     //BUTTOM UTILITIES
